d92adeff63d4d5e3ac891c45676d9e90a68fd578
Bulat Ahmetshin <Bulat.A.Ahmetshin@tatar.ru>
2022-04-17 00:00:56 +0300

    part of consumer

--- cnn/cnn_core.py
+++ cnn/cnn_core.py
@@ -1,23 +1,29 @@
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from models.classes import AbstractModel
+from models.classes import Pneumonia
+from models.classes import Tuberculosis
 
-model = load_model('cnn/pneumonia_categorical.h5')
 
-classes = ['Признаков пневномонии на снимке не обнаружено', 'Бактериальная пневмония', 'Вирусная пневмония']
+def get_cnn_prediction(filepath, illness):
+    model_class = globals()[illness]
+    model_ins = model_class()
+    if not isinstance(model_ins, AbstractModel):
+        return False
 
+    cnn_model = load_model(model_ins.cnn_model_path)
+    classes = model_ins.classes
 
-def get_cnn_prediction(filepath):
     img = image.load_img(filepath, target_size=(150, 150), color_mode="rgb")
     x = image.img_to_array(img)
     x = x.reshape(-1, 150, 150, 3)
 
-    prediction = model.predict(x).tolist()[0]
+    prediction = cnn_model.predict(x).tolist()[0]
 
     i = 0
     for diagnosis in prediction:
         if diagnosis == 1:
             prediction = classes[i]
-            break
         i += 1
 
     return prediction
